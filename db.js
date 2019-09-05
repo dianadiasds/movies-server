1775251fb22fdbb16bf1ccc4a0a3c4424d6da7dd
Diana Dias <dianadias@DESKTOP-DB8GNS3.localdomain>
2019-09-05 16:50:10 +0200

    ajusts on model and route

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 const db = new Sequelize(databaseUrl);
 
 db
-    .sync({ force: true })
+    .sync({ force: false})
     .then(() => console.log('Database schema updated'))
     .catch(console.error);
 
